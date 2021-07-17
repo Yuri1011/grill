aa99fa5b2a918d088b285ad5dedd044388f064ae
yury trypolski <yury1011.89@gmail.com>
2021-07-17 14:08:58 +0300

    create app grill

---
+++ src/AppGrillData.js
@@ -0,0 +1,8 @@
+import "./AppGrillData.css";
+import { Grill } from "./components/Grill";
+
+function AppGrillData() {
+  return <Grill />;
+}
+
+export default AppGrillData;
---
+++ src/components/Grill.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Grill() {
+  return <div>hello</div>;
+}
